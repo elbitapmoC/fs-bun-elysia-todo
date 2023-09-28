332b9ec93898587af7b0d0b467c201fc5dead7c2
Bazell Partridge <elbitapmoC@users.noreply.github.com>
2023-09-28 14:52:09 -0400

    save

--- src/ghAPI.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-async function fetchUser(user) {
-  const res = await axios.get(`https://api.github.com/users/${user}`);
-  return res.data;
-}
-
-export default fetchUser;
